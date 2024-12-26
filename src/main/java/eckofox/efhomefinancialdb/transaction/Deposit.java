02b247e99184c3eb20ed06e9f37a8cf93ed4de94
eckofox1981 <franklineck@pm.me>
2024-12-26 18:17:57 +0100

    worked on main screen

---
+++ src/main/java/eckofox/efhomefinancialdb/transaction/Deposit.java
@@ -0,0 +1,11 @@
+package eckofox.efhomefinancialdb.transaction;
+
+import eckofox.efhomefinancialdb.application.App;
+
+
+public class Deposit extends Transaction {
+
+    public Deposit(App app) {
+        super(app);
+    }
+}
